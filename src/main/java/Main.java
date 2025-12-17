7851b2354d2766551c0cf5814b061476e15ef1a1
lorena-salinas <lsali023@fiu.edu>
2025-12-16 19:15:01 -0500

    parent df82a5999884234405d70c4883aeb5bf69cd9d97 author lorena-salinas <lsali023@fiu.edu> 1765930501 -0500 committer lorena-salinas <lsali023@fiu.edu> 1766024537 -0500
    parent df82a5999884234405d70c4883aeb5bf69cd9d97
    author lorena-salinas <lsali023@fiu.edu> 1765930501 -0500
    committer lorena-salinas <lsali023@fiu.edu> 1766023859 -0500
    
    # This is a combination of 2 commits.
    # This is the 1st commit message:
    
    Executing external programs with arguments
    
    # This is the commit message #2:
    
    Executing external programs with arguments
    
    Executing external programs with arguments
    
    Executing external programs with arguments
    
    Executing external programs with arguments
    
    Executing external programs with arguments
    
    Executing external programs with arguments
    
    Executing external programs with arguments
    
    Executing external programs with arguments
    
    Executing external programs with arguments
    
    Executing external programs with arguments
    
    Executing external programs with arguments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,9 +10,10 @@ public class Main {
             System.out.print("$ ");
 
             Scanner scanner = new Scanner(System.in);
-            String command = scanner.nextLine();
-            String split[] = command.split(" ", 2);
-            command = split[0];
+            String input = scanner.nextLine();
+            // Separate the command name from the rest of the line
+            String split[] = input.split(" ", 2);
+            String command = split[0];
 
             if (command.equals("exit")) {
                 break;
@@ -30,30 +31,53 @@ public class Main {
                     System.out.println(command_or_file_name + " is a shell builtin");
                 }
                 else {
-                    // Get the PATH environment variable
-                    String path = System.getenv("PATH");
-                    boolean found = false;
-
-                    if (path != null) {
-                        // Split PATH into individual directories
-                        String[] directories = path.split(":");
-                        //Iterate over each
-                        for (String directory : directories) {
-                            File filePath = new File(directory, command_or_file_name); // construct full path directory/command_or_file_name
-                            // Check if it exists in the current directory and if it is executable
-                            if (filePath.exists() && Files.isExecutable(filePath.toPath())) {
-                                System.out.println(command_or_file_name + " is " + filePath.getAbsolutePath());
-                                found = true;
-                                break;
-                            }
-                        }
-                    } if (!found) {
+                    String foundFilePath = findExecutable(command_or_file_name);
+                    boolean found = foundFilePath != null;
+                    if (found == true) {
+                        System.out.println(command_or_file_name + " is " + foundFilePath);
+                    } else {
                         System.out.println(command_or_file_name + ": not found");
                     }
                 }
                 continue;
             }
-            System.out.println(command + ": command not found");
+
+            // For external programs
+            String[] command_args = input.split(" "); // each argument as a separate string
+
+            String program = command_args[0];
+            String prog_path = findExecutable(program);
+            if (prog_path == null) {
+                System.out.println(program + ": command not found");
+                continue;
+
+            }
+
+            ProcessBuilder builder = new ProcessBuilder(command_args);
+            builder.inheritIO(); // connects programs output to shell
+            Process process = builder.start();
+            process.waitFor();
+
+            }
         }
+
+    // Helper to locate executables in PATH
+    private static String findExecutable(String command) {
+        // Get the PATH environment variable
+        String path = System.getenv("PATH");
+
+        if (path != null) {
+            // Split PATH into individual directories
+            String[] directories = path.split(":");
+            //Iterate over each
+            for (String directory : directories) {
+                File filePath = new File(directory, command); // construct full path directory/command_or_file_name
+                // Check if it exists in the current directory and if it is executable
+                if (filePath.exists() && Files.isExecutable(filePath.toPath())) {
+                    return filePath.getAbsolutePath();
+                }
+            }
+        }
+        return null;
     }
 }
